f934db9090ff3d9ed1d5cff047ba156a6a2916b0
denniswanjiru <denniswanjiru71@gmail.com>
2018-06-22 08:50:32 +0300

    [Chore #158368335] COnfigure Babel and webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    app: "./src/app.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["babel-preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  devtool: "inline-source-map"
+};
